7125bc7c795c85267c4ab52fcc5705cd4e252b4b
Omar-Coder-dev <omarhafezhassan3@gmail.com>
2026-06-24 00:53:26 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("src/jobs.json"); // Matches your path perfectly!
+const router = jsonServer.router("src/jobs.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080;
 
@@ -9,3 +9,6 @@ server.use(router);
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
 });
+
+// Vercel needs this line to export the server instance
+module.exports = server;
